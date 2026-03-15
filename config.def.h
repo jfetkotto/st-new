087fb584e09fc3124834d38d697af5a8e7707af9
Sam Mitchell <jfetkotto@protonmail.com>
2026-03-15 10:57:49 +0000

    Zenbones

--- config.def.h
+++ config.def.h
@@ -96,24 +96,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#a89984", /* white   */
+	[0] = "#1C1917", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#DE6E7C", /* red     */
+	[2] = "#819B69", /* green   */
+	[3] = "#B77E64", /* yellow  */
+	[4] = "#6099C0", /* blue    */
+	[5] = "#B279A7", /* magenta */
+	[6] = "#66A5AD", /* cyan    */
+	[7] = "#B4BDC3", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#928374", /* black   */
-	[9]  = "#fb4934", /* red     */
-	[10] = "#b8bb26", /* green   */
-	[11] = "#fabd2f", /* yellow  */
-	[12] = "#83a598", /* blue    */
-	[13] = "#d3869b", /* magenta */
-	[14] = "#8ec07c", /* cyan    */
-	[15] = "#ebdbb2", /* white   */
+	[8]  = "#403833", /* black   */
+	[9]  = "#E8838F", /* red     */
+	[10] = "#8BAE68", /* green   */
+	[11] = "#D68C67", /* yellow  */
+	[12] = "#61ABDA", /* blue    */
+	[13] = "#CF86C1", /* magenta */
+	[14] = "#65B8C1", /* cyan    */
+	[15] = "#888F94", /* white   */
 };
 
 
